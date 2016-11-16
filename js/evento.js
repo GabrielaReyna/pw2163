899730093a82e3c20eae885a1c20e9728f78353a
GabrielaReyna <gabrielareynaflores@gmail.com>
2016-11-16 12:01:44 -0700

    Jquery y cositas de randomuser

---
+++ js/evento.js
@@ -0,0 +1,25 @@
+// DOM = modelo de objetos del documento
+var inicio = function() //main
+{
+	var dameclic = function()
+	{
+		$ .ajax ({
+ 		 url: ' https://randomuser.me/api/ ',
+ 		 dataType: ' json ',
+  		 success: function ( data ) {
+  		 	$("#txtNombre").val(data.results[0].name.first+ " "+data.results[0].name.last)
+  		 	$("#imgFoto").attr("src", data.results[0].picture.medium )	
+  		  console.log ( data.results[0].name.first+ " "+data.results[0].name.last);
+  }
+});
+	}
+	$("#dameClic").on("click",dameclic)
+}
+// Inicializar nuestro documento
+$(document).on("ready",inicio);
+
+// $(document).on("ready",function(){
+	// código o más funciones
+// });
+// funciones anonimas: Sin nombres, se pueden asignar a una variable. 
+// Si no se asignan a nada, sirven para ejecutarse una sola vez.
