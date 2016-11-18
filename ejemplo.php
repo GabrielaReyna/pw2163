56582c7ba2c4e7d51e58fb44c9fbea4802d8d323
GabrielaReyna <gabrielareynaflores@gmail.com>
2016-11-18 12:05:55 -0700

    primer codigo en php

---
+++ ejemplo.php
@@ -0,0 +1,16 @@
+<form action="ejemplo.php" method="POST">
+	<input type="hidden" name="txtOculto" id="guardaUsuario">
+	<input type="text" name="txtUsuario" id="txtUsuaruo">
+	<input type="text" name="txtUNombre" id="txtNombre">
+	<input type="password" name="txtClave" id="txtClave">
+	<select>
+		<option value="administrador">Administrador</option>
+		<option value="invitado">Invitado</option>
+		<option value="colado">Colado</option>
+
+	</select>"
+		<input type="submit" value="Enviar">
+</form>
+<?php 
+
+ ?>
