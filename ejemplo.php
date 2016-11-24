e10b2e2c29636b8a1608331cf5722acc20dc7286
GabrielaReyna <gabrielareynaflores@gmail.com>
2016-11-24 11:54:40 -0700

    Tabla con datos de consulta

--- ejemplo.php
+++ ejemplo.php
@@ -1,55 +1,4 @@
-<?php 
-	//Preguntar si los valores existen
-	$oculto=""; //Inicializamos las variables
-	$usuario="";
-	$nombre="";
-	$clave="";
-	$tipo="";
-	if(isset($_POST["txtOculto"])){//si trae valores
-		$oculto = $_POST["txtOculto"];
-		print "oculto";
-	}
-	if(isset($_POST["txtUsuario"])){//si trae valores
-		$usuario = $_POST["txtUsuario"];
-	}
-	if(isset($_POST["txtNombre"])){//si trae valores
-		$nombre = $_POST["txtNombre"];
-	}
-	if(isset($_POST["txtClave"])){//si trae valores
-		$clave= $_POST["txtClave"];
-	}
-	if(isset($_POST["txtTipo"])){//si trae valores
-		$tipo = $_POST["txtTipo"];
-	}
 
-	function guardaUsuario($usuario, $nombre, $clave, $tipo){
-		//Conectarse al servidor MySQL
-		
-		$conexion = mysql_connect("localhost", "root", ""); //Servidor, usuario y pass
-		//Seleccionamos la BD
-		mysql_select_db("bd2163");
-		$consulta = "insert into usuarios values('".$usuario."', '".$nombre."', '".$clave."', '".$tipo."')";
-		//Ejecutamos la conslta
-		mysql_query($consulta);
-		//preguntamos si hubo enserciòn
-		if(mysql_affected_rows() > 0){
-			print "Registro guardado";
-
-		}else{
-			print "No se pudo guardar el registro";
-		}
-	}	
-
-	switch ($oculto){
-		case 'guardaUsuario':
-			guardaUsuario($usuario, $nombre, $clave, $tipo);
-			break;
-		
-		default:
-			
-			break;
-	}
- ?>
  <h1>Alta de Usuarios</h1>
 <form action="ejemplo.php" method="POST">
 	<input type="hidden" name="txtOculto" value="guardaUsuario">
@@ -64,3 +13,26 @@
 	</select> <br>
 		<input type="submit" value="Enviar">
 </form>
+<hr>
+<?php
+	//conecto al servidor
+	$conexion = mysql_connect("localhost", "root", "");
+	mysql_select_db("bd2163");
+	$consulta = "select * from usuarios order by usuario";
+	$resultado = mysql_query($consulta); //ejecuta la consulta
+	$tabla = "<table border=1>";
+	$tabla.="<tr>";
+	$tabla.="<th>Usuario</th> <th>Nombre</th> <th>Clave</th> <th>Tipo</th>";
+	$tabla.="</tr>";
+	while($registro = mysql_fetch_array($resultado);){ //Mientras se pueda hacer la asignaciòn
+		$tabla.="<tr>";
+		$tabla.="<td>".$registro["usuario"]."</td>";
+		$tabla.="<td>".$registro["nombre"]."</td>";
+		$tabla.="<td>".$registro["clave"]."</td>";
+		$tabla.="<td>".$registro["tipousuario"]."</td>";
+		$tabla.="</tr>";
+	}
+	$tabla.="</table>";
+	print $tabla;
+	
+?>
