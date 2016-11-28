c35adf3d9dddede128e6c3f18dbb7090d1118712
GabrielaReyna <gabrielareynaflores@gmail.com>
2016-11-28 12:02:27 -0700

    muchas cosas wuuuu

---
+++ usuarios/js/eventos.js
@@ -0,0 +1,39 @@
+var inicioUsuarios = function(){
+
+
+	var validaUsuario = function(){
+		//Extraer los datos de los input en el HTML
+		var usuario= $("#txtUsuario").val();
+		var clave = $("#txtClave").val();
+		//Preparar los paràmetros para ajax
+
+		var parametros = "opcion=valida"+
+						"&usuario="+usuario+
+						"&clave="+clave+
+						"&id="+Math.random();
+		//Hacemos la peticion remota
+		$.ajax({
+			cache:false,
+			type:"POST",
+			dataType:"json",
+			url: "php/utilerias.php"
+			datos:parametros;
+			success: function(response){
+				//Si todo sale bien
+			},
+			error: function(xhr, ajaxOptions, throwError){
+				//si todo sale mal
+			}
+		});
+
+		//validamos que no estén vacíos
+		if(usuario!=""&&clave!=""){
+
+		}else{
+			alert("Usuario y clave son obligatorios")
+		}
+	}
+	$("#btnValidaUsuario").on("click", validaUsuario);
+}
+//evento inicial
+$(document).on("ready", inicioUsuarios);
